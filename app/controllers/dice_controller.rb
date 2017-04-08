0d74271433ba394190de1cba8b5671fc528708c7
cedretaber <cedretaber@gmail.com>
2017-04-08 18:17:07 +0900

    bump

--- app/controllers/dice_controller.rb
+++ app/controllers/dice_controller.rb
@@ -3,6 +3,22 @@ class DiceController < ApplicationController
   end
 
   def dice
-    render json: Connpass.find_event("ボードゲーム")
+    video = Youtube::Video.offset(rand(Youtube::Video.count)).first
+
+    events = Connpass.find_event("ボードゲーム")
+
+    response = {
+      video: { id: video&.video_id },
+      relatedVideo: video&.video_related&.sample(3)&.map { |related|
+        rel_video = Youtube::Video.find(related.to_youtube_video_id)
+        {
+          id: rel_video.video_id,
+          thumnail: rel_video.thumnail_image_url
+        }
+      },
+      eventPages: events&.sample(3)
+    }
+    
+    render json: response
   end
 end
