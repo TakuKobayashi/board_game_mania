6f99a80fb53f3478e2428bf2efe0e40e3ceb7d22
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2017-04-08 18:51:01 +0900

    サニタイズ処理

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,14 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  def self.basic_sanitize(text)
+    #絵文字を除去
+    sanitized_word = text.encode('SJIS', 'UTF-8', invalid: :replace, undef: :replace, replace: '').encode('UTF-8')
+    #全角半角をいい感じに整える
+    sanitized_word = Charwidth.normalize(sanitized_word)
+    
+    # 余分な空欄を除去
+    sanitized_word.strip!
+    return sanitized_word
+  end
 end
