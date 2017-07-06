82e115b9272525844e1c3cb7c394d7f9b9f461d5
TakuKobayshi <>
2017-07-06 14:39:39 +0900

    使っていないルーティング削除

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,3 @@
 Rails.application.routes.draw do
   get '/' => 'dice#index'
-
-  get '/dice' => 'dice#dice'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
