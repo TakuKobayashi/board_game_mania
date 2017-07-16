cee0ec7e1f2ca74b514def522210d4959a0097b6
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2017-07-16 17:05:29 +0900

    読み込めないので...

--- app/controllers/dice_controller.rb
+++ app/controllers/dice_controller.rb
@@ -1,6 +1,6 @@
 class DiceController < ApplicationController
   def index
-    @video = Youtube::Video.find_by!(id: rand(Youtube::Video.last.id))
+    @video = Youtube::Video.where("id >= ?", rand(Youtube::Video.last.id)).first
     @events = Event.where("? < started_at AND started_at < ?", Time.current, 1.year.since).order("started_at ASC").select{|event| event.hackathon_event? }
   end
 end
