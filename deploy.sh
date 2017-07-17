6f005ec013960471228245cf5359637d3a97e266
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2017-07-17 23:20:04 +0900

    cacheとかあるから、asset precompileはやり直す方向で

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,8 @@
 git pull
 bundle install
 RAILS_ENV=production rails db:migrate
-RAILS_ENV=production bundle exec rake assets:precompile
+RAILS_ENV=production rails assets:clean
+RAILS_ENV=production rails assets:precompile
 whenever --update-crontab
 kill -9 `cat tmp/pids/server.pid`
 SECRET_KEY_BASE=$(rake secret) rails server -e production -p 3010 -d
