0473d37fd96b8b5f112cb32ddd881ff436953cf0
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2017-07-21 02:29:29 +0900

    github callbackの設定

--- app/controllers/github_controller.rb
+++ app/controllers/github_controller.rb
@@ -1,2 +1,9 @@
 class GithubController < ApplicationController
+  protect_from_forgery
+
+  def hook
+    params.permit!
+    logger.info param
+    head(:ok)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   get '/' => 'dice#index'
+
+  resource :github, controller: :github, only: [] do
+    post 'hook'
+  end
 end
