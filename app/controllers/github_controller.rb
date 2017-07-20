6ac90edce7bc0f819ec6914cc4a362e0f73f0df8
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2017-07-21 03:29:54 +0900

    専用のshellを作って実行

--- app/controllers/github_controller.rb
+++ app/controllers/github_controller.rb
@@ -6,7 +6,7 @@ class GithubController < ApplicationController
     logger.info params
     call_hash = JSON.parse(params[:payload])
     if call_hash["repository"]["full_name"] == "TakuKobayashi/board_game_mania"
-      system("nohup cd " + Rails.root.to_s + " | sh " + Rails.root.to_s + "/deploy.sh")
+      system("nohup cd " + Rails.root.to_s + " | sh " + Rails.root.to_s + "/deploy_from_system.sh")
     end
     head(:ok)
   end
