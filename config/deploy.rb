386272b9d8b2a680c264383dd61e2328f92e2315
TakuKobayshi <>
2017-07-21 12:19:50 +0900

    capistranoの設定を反映(途中まで)

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,33 @@
 # config valid only for current version of Capistrano
 lock "3.8.2"
 
-set :application, "my_app_name"
-set :repo_url, "git@example.com:me/my_repo.git"
+set :application, "board_game_mania"
+set :repo_url, "https://github.com/TakuKobayashi/board_game_mania.git"
+set :branch, fetch(:stage)
+set :deploy_to, "/app/project/board_game_mania"
+set :keep_releases, 3
+
+set :linked_files, [".env", "config/database.yml", "config/apiconfig.yml"]
+set :linked_dirs, ["bin", "log", "tmp/pids", "tmp/sockets", "tmp/private", "public", "vendor/bundle"]
+
+# Rails config
+set :rails_env, fetch(:stage)
+# auto migration
+set :migration_role, :root
+# check difference and skip if no change
+set :conditionally_migrate, true
+
+# Bundler config
+set :bundle_path, -> { shared_path.join("bundle") }
+set :bundle_binstubs, -> { shared_path.join("bin") }
+set :bundle_without, (["development", "test", "production"] - [fetch(:stage).to_s]).join(" ")
+set :bundle_flags, "--deployment --quiet --full-index"
+
+# Whenever config
+# auto update crontab
+set :whenever_roles, :root
+set :whenever_environment, fetch(:stage).to_s
+set :whenever_identifier, ->{ "#{fetch(:application)}_#{fetch(:stage)}" }
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
--- config/deploy/development.rb
+++ config/deploy/development.rb
@@ -7,17 +7,7 @@
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
-# Server Roles
-role :root, %w{root@tk2-219-19169.vs.sakura.ne.jp}
-set :password, ask('Server password', nil)
 
-# RVM Config
-set :scm, :git
-set :rvm_type, :system
-set :rvm_ruby_version, "2.4.1@board_game_mania"
-
-set :branch, ENV['BRANCH'] || ENV['branch'] || "master"
-set :linked_files, [".env", "config/database.yml"]
 
 # role-based syntax
 # ==================
