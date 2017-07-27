c8314ca2f4f9c8cc0d3638b491e1679ae2408946
TakuKobayshi <>
2017-07-27 16:49:37 +0900

    log rotate

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,6 +15,8 @@ Rails.application.configure do
   # Rake tasks automatically ignore this option for performance.
   config.eager_load = true
 
+  config.logger = Logger.new("log/production.log", 'daily')
+
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
