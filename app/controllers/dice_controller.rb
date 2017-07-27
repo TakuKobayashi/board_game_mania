f2e6b6d7410d5cab67d2f86a26483543b3e6236f
TakuKobayshi <>
2017-07-27 11:24:28 +0900

    youtubenのvideoで引っ張ってくるルールを変更

--- app/controllers/dice_controller.rb
+++ app/controllers/dice_controller.rb
@@ -1,7 +1,7 @@
 class DiceController < ApplicationController
   def index
-    plane_videos = Youtube::Video.where(is_related: false)
-    @video = plane_videos.where("id >= ?", rand(plane_videos.last.id)).first
+    video_id = Youtube::VideoTag.where(tag: Event::BOARDGAME_KEYWORDS).pluck(:youtube_video_id).uniq.sample
+    @video = Youtube::Video.find_by(id: video_id)
     @events = Event.where("? < started_at AND started_at < ?", Time.current, 1.year.since).sample(5).sort_by(&:started_at)
   end
 end
