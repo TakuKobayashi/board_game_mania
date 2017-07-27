d19b056c354cb4fd13dcd616b101f80c191f9aad
TakuKobayshi <>
2017-07-27 09:53:59 +0900

    一旦タイトルで絞り込むのはやめる

--- app/controllers/dice_controller.rb
+++ app/controllers/dice_controller.rb
@@ -2,6 +2,6 @@ class DiceController < ApplicationController
   def index
     plane_videos = Youtube::Video.where(is_related: false)
     @video = plane_videos.where("id >= ?", rand(plane_videos.last.id)).first
-    @events = Event.where("? < started_at AND started_at < ?", Time.current, 1.year.since).select{|event| event.boardgame_event? }.sample(5).sort_by(&:started_at)
+    @events = Event.where("? < started_at AND started_at < ?", Time.current, 1.year.since).sample(5).sort_by(&:started_at)
   end
 end
