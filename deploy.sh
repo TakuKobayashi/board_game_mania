3d46c23e907a7c2d3c6e74d26b92dcc80a6483e7
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2018-10-13 22:48:51 +0900

    bundle exec を追加しました

--- deploy.sh
+++ deploy.sh
@@ -2,10 +2,10 @@
 
 git pull
 bundle install
-RAILS_ENV=production rails db:migrate
-RAILS_ENV=production rails assets:clean
-RAILS_ENV=production rails assets:precompile --trace
-whenever --update-crontab
-spring stop
+RAILS_ENV=production bundle exec rails db:migrate
+RAILS_ENV=production bundle exec rails assets:clean
+RAILS_ENV=production bundle exec rails assets:precompile --trace
+bundle exec whenever --update-crontab
+bundle exec spring stop
 kill -9 `cat tmp/pids/server.pid`
-SECRET_KEY_BASE=$(rake secret) RAILS_SERVE_STATIC_FILES=true rails server -e production -p 3010 -d
+SECRET_KEY_BASE=$(rake secret) RAILS_SERVE_STATIC_FILES=true bundle exec rails server -e production -p 3010 -d
