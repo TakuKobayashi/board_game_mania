e40477d2c59fcff95e35e872c6ad1a2e8cb1d54e
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2018-10-15 03:19:31 +0900

    rails 5.2.1にバージョンアップ

--- config/application.rb
+++ config/application.rb
@@ -1,16 +1,6 @@
 require_relative 'boot'
 
-require "rails"
-# Pick the frameworks you want:
-require "active_model/railtie"
-require "active_job/railtie"
-require "active_record/railtie"
-require "action_controller/railtie"
-require "action_mailer/railtie"
-require "action_view/railtie"
-require "action_cable/engine"
-require "sprockets/railtie"
-# require "rails/test_unit/railtie"
+require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -18,17 +8,12 @@ Bundler.require(*Rails.groups)
 
 module BoardGameMania
   class Application < Rails::Application
-    config.load_defaults 5.1
-
-    config.time_zone = "Tokyo"
-    config.active_record.default_timezone = :local
-
-    config.encoding = "utf-8"
-    config.i18n.default_locale = :ja
-    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 5.2
 
     # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    # Application configuration can go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded after loading
+    # the framework and any gems in your application.
   end
 end
--- config/initializers/session_store.rb
+++
@@ -1,3 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-Rails.application.config.session_store :cookie_store, key: '_board_game_mania_session'
