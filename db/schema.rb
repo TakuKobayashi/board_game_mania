0255dba76f049056ea299b25131122b394660cde
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2019-10-16 17:17:13 +0900

    update schema.

--- db/schema.rb
+++ db/schema.rb
@@ -2,17 +2,17 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170718015852) do
+ActiveRecord::Schema.define(version: 2019_10_16_074748) do
 
-  create_table "events", id: :integer, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "events", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin", force: :cascade do |t|
     t.string "event_id"
     t.string "type"
     t.string "title", null: false
@@ -24,8 +24,8 @@ ActiveRecord::Schema.define(version: 20170718015852) do
     t.integer "limit_number"
     t.string "address", null: false
     t.string "place", null: false
-    t.float "lat", limit: 24
-    t.float "lon", limit: 24
+    t.float "lat"
+    t.float "lon"
     t.string "owner_id"
     t.string "owner_nickname"
     t.string "owner_name"
@@ -36,7 +36,7 @@ ActiveRecord::Schema.define(version: 20170718015852) do
     t.index ["title"], name: "index_events_on_title"
   end
 
-  create_table "twitter_bots", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "twitter_bots", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin", force: :cascade do |t|
     t.string "tweet", null: false
     t.string "tweet_id", null: false
     t.string "from_type"
@@ -48,21 +48,21 @@ ActiveRecord::Schema.define(version: 20170718015852) do
     t.index ["tweet_id"], name: "index_twitter_bots_on_tweet_id"
   end
 
-  create_table "youtube_video_relateds", id: :integer, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "youtube_video_relateds", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin", force: :cascade do |t|
     t.integer "youtube_video_id", null: false
     t.integer "to_youtube_video_id", null: false
     t.index ["to_youtube_video_id"], name: "index_youtube_video_relateds_on_to_youtube_video_id"
     t.index ["youtube_video_id"], name: "index_youtube_video_relateds_on_youtube_video_id"
   end
 
-  create_table "youtube_video_tags", id: :integer, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "youtube_video_tags", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin", force: :cascade do |t|
     t.integer "youtube_video_id", null: false
     t.string "tag", null: false
     t.index ["tag"], name: "index_youtube_video_tags_on_tag"
     t.index ["youtube_video_id", "tag"], name: "index_youtube_video_tags_on_youtube_video_id_and_tag", unique: true
   end
 
-  create_table "youtube_videos", id: :integer, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "youtube_videos", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin", force: :cascade do |t|
     t.string "video_id", default: "", null: false
     t.integer "youtube_channel_id"
     t.integer "youtube_category_id"
